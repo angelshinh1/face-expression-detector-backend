cdea2f15831bdd9ada5f0372f259143f24d56424
angelshinh1 <angelshinh2023@gmail.com>
2025-04-23 23:49:06 -0400

    added procfile

--- app.py
+++ app.py
@@ -242,6 +242,4 @@ def make_pred():
 
 
 if __name__ == "__main__":
-    port = 8080
-    print(f"Starting server on port {port}...")
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
